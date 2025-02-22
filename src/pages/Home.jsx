a92e1aab40534d92524cabfeadccf16a95233d54
TrinhTT8 <ttiffany1608@gmail.com>
2025-02-22 15:48:13 -0600

    Add existing project files to Movie List project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import MovieCard from "../components/MovieCard"
+import {useState} from "react"
+import "../css/Home.css"
+
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        {id: 1, title: "John Wick", release_date: "2020"},
+        {id: 2, title: "The Gorge", release_date: "2025"},
+        {id: 3, title: "The Batman Rises", release_date: "2018"},
+        {id: 4, title: "Arcane", release_date: "2020"},
+    ];
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
+
+
+    return <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input 
+            type="text" 
+            placeholder="Search for movies..." 
+            className="search-input"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">Search</button>
+        </form>
+
+        <div className="movie-grid">
+            {movies.map(
+                (movie) => 
+ 
+                    (<MovieCard movie={movie} key={movie.id} />
+                    )
+            )}
+        </div>
+    </div>
+}
+
+export default Home;
